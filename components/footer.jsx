87c713c0565b3bd8f50fc4c1d1750214563f2ebe
GrantLicata <grant.licata@gmail.com>
2025-01-01 10:23:14 -0800

    Footer improved

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,10 +1,36 @@
-export const Footer = () => {
+import Link from "next/link";
+import { ContainerInner, ContainerOuter } from "./container";
+
+function NavLink({ href, children }) {
   return (
-    <div className="flex sm:justify-between items-center w-full p-6 bg-gray-600 z-50">
-      <p className="text-muted-foreground text-sm font-medium text-center sm:text-left w-full">
-        © 2023 Storynote, Inc.
-      </p>
-      <div className="md:ml-auto w-full justify-between hidden sm:flex sm:justify-end items-center gap-x-2 text-muted-foreground"></div>
-    </div>
+    <Link
+      href={href}
+      className="transition hover:text-teal-500 dark:hover:text-teal-400"
+    >
+      {children}
+    </Link>
   );
-};
+}
+
+export function Footer() {
+  return (
+    <footer className=" flex-none">
+      <ContainerOuter>
+        <div className="border-t border-zinc-100 pb-16 pt-10 ">
+          <ContainerInner>
+            <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+              <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
+                <NavLink href="/about">About</NavLink>
+                <NavLink href="/ratings">Ratings</NavLink>
+                <NavLink href="/contact">Contact</NavLink>
+              </div>
+              <p className="text-sm text-zinc-400 dark:text-zinc-500">
+                &copy; {new Date().getFullYear()} GranTech. All rights reserved.
+              </p>
+            </div>
+          </ContainerInner>
+        </div>
+      </ContainerOuter>
+    </footer>
+  );
+}

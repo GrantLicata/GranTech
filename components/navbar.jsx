f8a6a1de2673f1a5a6b3c12de101b7b6cf9792fd
GrantLicata <grant.licata@gmail.com>
2025-01-01 10:54:43 -0800

    CN utility fixed to work with navbar component

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -2,15 +2,23 @@
 
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
+import { useEffect } from "react";
 
 export const Navbar = () => {
   const scrolled = useScrollTop();
+
+  useEffect(() => {
+    console.log(scrolled);
+  }, [scrolled]);
+
   return (
     <div
       className={cn(
-        "z-50 bg-gray-600 fixed top-0 flex items-center w-full p-6",
+        "z-50 bg-background fixed top-0 flex items-center w-full p-6",
         scrolled && "border-b shadow-sm"
       )}
-    ></div>
+    >
+      <h1>grantech</h1>
+    </div>
   );
 };

594b1e59ebdebd889e26789d7af397f2895c38e3
GrantLicata <grant.licata@gmail.com>
2025-01-01 10:20:32 -0800

    Standard container component added

--- app/(main)/(routes)/products/page.jsx
+++ app/(main)/(routes)/products/page.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function ProductsPage() {
-  return <div className="min-w-full">page</div>;
+  return <div className="h-dvh min-w-full">page</div>;
 }
--- components/layout.jsx
+++ components/layout.jsx
@@ -1,16 +1,11 @@
 import { Footer } from "./footer";
-import { Header } from "./header";
+import { Navbar } from "./navbar";
 
 export function Layout({ children }) {
   return (
     <>
-      <div className="fixed inset-0 flex justify-center sm:px-8">
-        <div className="flex w-full max-w-7xl lg:px-8">
-          <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
-        </div>
-      </div>
       <div className="relative flex w-full flex-col">
-        <Header />
+        <Navbar />
         <main className="flex-auto">{children}</main>
         <Footer />
       </div>
